13ea3790dfc401e432751ead255702e9329f79c2
emilyparkes <emilycoco@me.com>
2022-01-12 11:42:53 +1300

    initial example

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,21 +1,9 @@
 import React from 'react'
-import {Route} from 'react-router-dom'
-
-import Home from './Home'
-import Nav from './Nav'
-import Puppies from './Puppies'
-import Pup from './Pup'
-import Contact from './Contact'
 
 function App () {
   return (
     <>
       <div>App</div>
-      <Route exact path='/' component={Home} />
-      <Route path='/' component={Nav} />
-      <Route path='/puppies' component={Puppies} />
-      <Route path='/puppy/:id' component={Pup} />
-      <Route path='/contact' component={Contact} />
     </>
   )
 }

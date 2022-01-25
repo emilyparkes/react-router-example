2fc2b44462821b2a527118676b76a7394ca72d28
emily parkes <emilycoco@me.com>
2022-01-25 23:23:07 +1300

    initial app

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,9 +1,34 @@
 import React from 'react'
+import puppies from '../../data/puppies'
 
 function App () {
   return (
     <>
-      <div>App</div>
+      <div className='title'>
+        <h2>Welcome to my Doggo Directory</h2>
+      </div>
+
+      <div className='puppy-list'>
+        <h3>All Puppies</h3>
+        <ul>
+          {puppies.map(pup => {
+            return <li key={pup.id}>
+              {pup.name}
+            </li>
+          }
+          )}
+        </ul>
+      </div>
+
+      <div>
+        <h3>Contact to see puppers IRL:</h3>
+        <ul>
+          <li>275 Cuba Street, Poneke</li>
+          <li>hello@devacademy.co.nz</li>
+          <li>Via DM on Slack</li>
+          <li>Bren, Freya, Tom and Casey</li>
+        </ul>
+      </div>
     </>
   )
 }
